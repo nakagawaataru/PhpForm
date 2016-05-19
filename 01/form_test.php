32a9d655fc0c7f2f36b7adcf2d396a9ec77cd09e
nakagawa ataru <faran.5mo@gmail.com>
2016-05-19 13:56:16 +0900

    20160519

---
+++ 01/form_test.php
@@ -0,0 +1,10 @@
+<form action = "contact.phpresult.php" method="post">
+
+    <input type="text" name="k" /><br>
+    <label type="text">名</label><br>
+    <input type="author" type="text">
+
+<input type="submit">
+
+
+</form>
