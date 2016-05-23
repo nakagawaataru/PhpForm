ed1afb1272ecc2d931c34ebf22e20b015ece06d8
nakagawa ataru <faran.5mo@gmail.com>
2016-05-23 14:59:16 +0900

    20160523

--- 02/form_test.php
+++ 02/form_test.php
@@ -6,16 +6,15 @@
 <form action = "contact.phpresult.php" method="post">
 
     <h1>お問い合わせ</h1>
-
     <div id = "name_left">姓</div>
     <div id = "name_right">
-        <input type="text" name="k" >
+        <input type="text" name="k" placeholder = "例）山田" value="">
     </div>
     <div class="clear_box"></div>
 
     <div id = "name_left">名</div>
     <div id = "name_right">
-        <input type="text" name="n" >
+        <input type="text" name="n" placeholder = "例）太郎" value="">
     </div>
     <div class="clear_box"></div>
 
@@ -30,21 +29,21 @@
 
     <div id = "name_left">住所</div>
     <div id = "name_right">
-    <input type="text" name="adress" >
+    <input type="text" name="adress" placeholder = "例）東京都〇〇区〇-〇-〇" value="">
     </div>
     <div class="clear_box"></div>
 
     <div id = "name_left">電話番号</div>
     <div id = "name_right">
-    <input type="text" name="tell" >-
-    <input type="text" name="tell2" >-
-    <input type="text" name="tell3" >
+    <input type="text" name="tell" placeholder = "例）090" value="">-
+    <input type="text" name="tell2" placeholder = "例）000" value="">-
+    <input type="text" name="tell3" placeholder = "例）000" value="">
     </div>
 
     <div id = "name_left">メールアドレス</div>
     <div id = "name_right">
-    <input type="text" name="mail" >@
-    <input type="text" name="mail2" >
+    <input type="text" name="mail" placeholder = "例）test" value="">@
+    <input type="text" name="mail2" placeholder = "例）gmail.com" value="">
     </div>
     <div class="clear_box"></div>
 
@@ -69,7 +68,7 @@
 
     <div id = "name_left">質問内容</div>
     <div id = "name_right">
-    <textarea rows="2" cols="40" name="question"></textarea>
+    <textarea rows="2" cols="40" name="question" placeholder = "例）パソコンが壊れました" value=""></textarea>
     </div>
     <div class="clear_box"></div>
 
