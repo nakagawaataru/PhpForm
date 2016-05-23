8b685999a4c4388febfb43d06d71ba2ab9ee6541
nakagawa ataru <faran.5mo@gmail.com>
2016-05-23 13:41:54 +0900

    20160523

--- 02/form_test.php
+++ 02/form_test.php
@@ -1,33 +1,81 @@
+<meta charset="utf-8">
+<link rel = "stylesheet" href = "test.css">
+<body>
+<div id="flame">
+
 <form action = "contact.phpresult.php" method="post">
 
-<center>
-    <span style="font-size:18px;">
     <h1>お問い合わせ</h1>
 
-    <link rel = "stylesheet" href = "test.css">
-    <div>姓:<input type="text" name="k" ></div>
-    <div>名:<input type="text" name="n"></div>
+    <div id = "name_left">姓</div>
+    <div id = "name_right">
+        <input type="text" name="k" >
+    </div>
+    <div class="clear_box"></div>
 
-    性別:<input type="radio" name="rdo" value="男">男性
-    <input type="radio" name="rdo" value="女">女性:
-    <input type="radio" name="rdo" value="不明">不明<br>
-    <div>住所:<input type="text" name="adress" ><br></div>
-    <div>電話番号:<input type="text" name="tell" >-<input type="text" name="tell2" >-<input type="text" name="tell3" ><br></div>
-    <div>メールアドレス:<input type="text" name="mail" >@<input type="text" name="mail2" ><br></div>
-    どこで知ったか:<input type="checkbox" name="cbx" value="雑誌">雑誌
+    <div id = "name_left">名</div>
+    <div id = "name_right">
+        <input type="text" name="n" >
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">性別</div>
+    <div id = "name_right">
+    <input type="radio" name="rdo" value="男">男性
+    <input type="radio" name="rdo" value="女">女性
+    <input type="radio" name="rdo" value="不明">不明
+    </div>
+    <div class="clear_box"></div>
+
+
+    <div id = "name_left">住所</div>
+    <div id = "name_right">
+    <input type="text" name="adress" >
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">電話番号</div>
+    <div id = "name_right">
+    <input type="text" name="tell" >-
+    <input type="text" name="tell2" >-
+    <input type="text" name="tell3" >
+    </div>
+
+    <div id = "name_left">メールアドレス</div>
+    <div id = "name_right">
+    <input type="text" name="mail" >@
+    <input type="text" name="mail2" >
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">どこで知ったか</div>
+    <div id = "name_right">
+    <input type="checkbox" name="cbx" value="雑誌">雑誌
     <input type="checkbox" name="cbx" value="インターネット">インターネット
-    <input type="checkbox" name="cbx" value="学校">学校<br>
-    質問のカテゴリ:<select name="slt">
+    <input type="checkbox" name="cbx" value="学校">学校
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">質問のカテゴリ</div>
+    <div id = "name_right">
+    <select name="slt">
         <option>クレーム</option>
         <option>相談</option>
         <option>感想</option>
         <option>その他</option>
-    </select><br>
-    質問内容:<textarea rows="4" cols="40" name="question"></textarea><br>
+    </select>
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">質問内容</div>
+    <div id = "name_right">
+    <textarea rows="2" cols="40" name="question"></textarea>
+    </div>
+    <div class="clear_box"></div>
 
     <input type="submit">
     <input type="reset" >
 
-</span>
-</center>
 </form>
+</div>
+</body>
