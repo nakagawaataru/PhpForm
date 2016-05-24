8bcde1c2b01cc560018fae270d912c3280cf05a8
nakagawa ataru <faran.5mo@gmail.com>
2016-05-24 16:04:09 +0900

    20160524

--- 02/contact.php
+++ 02/contact.php
@@ -1,72 +1,95 @@
-<?php
-/*____________________________________
-echo $_POST['surname'];
-echo $_POST['name']. "<br>";
-echo $_POST['rdo'] . "<br>";
-echo $_POST['adress']. "<br>";
-echo $_POST['tell'] . "-";
-echo $_POST['tell2'] . "-";
-echo $_POST['tell3']. "<br>";
-echo $_POST['mail'] . "@";
-echo $_POST['mail2'] . "<br>";
-echo $_POST['cbx'] . "<br>";
-echo $_POST['slt'] . "<br>";
-echo $_POST['question'] . "<br>";
-_______________________________________
-*/
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $surname = $_POST["surname"];
-    $name = $_POST["name"];
-    $adress = $_POST["adress"];
-    $tell = $_POST["tell"];
-    $tell2 = $_POST["tell2"];
-    $tell3 = $_POST["tell3"];
-    $mail = $_POST["mail"];
-    $mail2 = $_POST["mail2"];
-    $cbx = $_POST["cbx"];
-    $slt = $_POST["slt"];
-    $question = $_POST["question"];
-    /*$question = nl2br($question);*/
-
-}
-?>
 <!DOCTYPE html>
 <html>
-
 <head>
-<link rel="stylesheet" href="test.css">
 <meta charset="utf-8">
-
-<body>
-
-    <div id = "flame">
-
-        <h1>送信確認画面</h1>
-
-        <div id = "name_left">姓名</div>
-        <div id = "name_right">
-            <?php echo $surname . '&nbsp;&nbsp;' . $name; ?>
-        </div>
-        <div class="clear_box"></div>
-
-        <div id = "name_left">性別</div>
-        <div id = "name_right">
-            <?php
-                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                    if ($_POST["rdo"] == "男性") {
-                        print "男性";
-                    }
-                    elseif ($_POST["rdo"] == "女性") {
-                        print "女性";
-                    }
-                    elseif ($_POST["rdo"] == "不明") {
-                        print "不明";
-                    }
-                }
-             ?>
-         </div>
-            <div class="clear_box"></div>
-
-</body>
+<link rel = "stylesheet" href = "test.css">
+<title>お問い合わせ</title>
 </head>
+<body>
+<div id="flame">
+
+<form action = "result.php" method="post">
+
+    <h1>お問い合わせ</h1>
+    <div id = "name_left">姓<div id=br>必須</div></div>
+    <div id = "name_right">
+        <input type="text" name="surname" placeholder = "例）山田" value="" required="required">
+        <div id="errormsg_姓" class="mfp_err"></div>
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">名<div id=br>必須</div></div>
+    <div id = "name_right">
+        <input type="text" name="name" placeholder = "例）太郎" value="" required="required">
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">性別<div id=br>必須</div></div>
+    <div id = "name_right">
+    <input type="radio" name="rdo" value="0"  checked id="m">
+    <label for="m">男性</label>
+    <input type="radio" name="rdo" value="1" id = "w">
+    <label for="w">女性</label>
+    <input type="radio" name="rdo" value="2" id="u">
+    <label for="u">不明</label>
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">住所<div id=br>必須</div></div>
+    <div id = "name_right">
+    <input type="text" name="adress" placeholder = "例）東京都〇〇区〇-〇-〇" value=""　required="required">
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">電話番号<div id=br>必須</div></div>
+    <div id = "name_right">
+    <input type="text" name="tell" placeholder = "例）090" value="" required="required">-
+
+    <input type="text" name="tell2" placeholder = "例）000" value="" required="required">-
+
+    <input type="text" name="tell3" placeholder = "例）000" value="" required="required">
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">メールアドレス<div id=br>必須</div></div>
+    <div id = "name_right">
+    <input type="text" name="mail" placeholder = "例）test" value="" required="required">@
+    <input type="text" name="mail2" placeholder = "例）gmail.com" value="" required="required">
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">どこで知ったか(複数可)</div>
+    <div id = "name_right">
+    <input type="checkbox" name="cbx[0]" id = "c" value="雑誌">
+    <label for="c">雑誌</label>
+    <input type="checkbox" name="cbx[1]" id = "b" value="インターネット">
+    <label for="b">インターネット</label>
+    <input type="checkbox" name="cbx[2]" value="学校" id = "x">
+    <label for="x">学校</label>
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">質問のカテゴリ<div id=br>必須</div></div>
+    <div id = "name_right">
+    <select name="slt">
+        <option>クレーム</option>
+        <option>相談</option>
+        <option>感想</option>
+        <option>その他</option>
+    </select>
+    </div>
+    <div class="clear_box"></div>
+
+    <div id = "name_left">質問内容<div id=br>必須</div></div>
+    <div id = "name_right">
+    <textarea rows="2" cols="40" name="question" placeholder = "例）パソコンが壊れました" value="" required="required"></textarea>
+    </div>
+    <div class="clear_box"></div>
+
+    <input type="submit">
+    <input type="reset" >
+
+</form>
+</div>
+</body>
 </html>
--- 02/result.php
+++ 02/result.php
@@ -1,94 +1,87 @@
+<?php
+/*____________________________________
+echo $_POST['surname'];
+echo $_POST['name']. "<br>";
+echo $_POST['rdo'] . "<br>";
+echo $_POST['adress']. "<br>";
+echo $_POST['tell'] . "-";
+echo $_POST['tell2'] . "-";
+echo $_POST['tell3']. "<br>";
+echo $_POST['mail'] . "@";
+echo $_POST['mail2'] . "<br>";
+echo $_POST['cbx'] . "<br>";
+echo $_POST['slt'] . "<br>";
+echo $_POST['question'] . "<br>";
+_______________________________________
+*/
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $surname = $_POST["surname"];
+    $name = $_POST["name"];
+    $rdo = $_POST["rdo"];
+    $adress = $_POST["adress"];
+    $tell = $_POST["tell"];
+    $tell2 = $_POST["tell2"];
+    $tell3 = $_POST["tell3"];
+    $mail = $_POST["mail"];
+    $mail2 = $_POST["mail2"];
+    $cbx = $_POST["cbx"];
+    $slt = $_POST["slt"];
+    $question = $_POST["question"];
+    /*$question = nl2br($question);*/
+
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
+<link rel="stylesheet" href="test.css">
 <meta charset="utf-8">
-<link rel = "stylesheet" href = "test.css">
-<title>お問い合わせ</title>
-</head>
+
 <body>
-<div id="flame">
 
-<form action = "contact.phpresult.php" method="post">
+    <div id = "flame">
 
-    <h1>お問い合わせ</h1>
-    <div id = "name_left">姓<div id=br>必須</div></div>
-    <div id = "name_right">
-        <input type="text" name="surname" placeholder = "例）山田" value="" required="required">
-        <div id="errormsg_姓" class="mfp_err"></div>
-    </div>
-    <div class="clear_box"></div>
+        <h1>送信確認画面</h1>
 
-    <div id = "name_left">名<div id=br>必須</div></div>
-    <div id = "name_right">
-        <input type="text" name="name" placeholder = "例）太郎" value="" required="required">
-    </div>
-    <div class="clear_box"></div>
+        <div id = "name_left">姓名</div>
+        <div id = "name_right">
+            <?php echo $surname . '&nbsp;&nbsp;' . $name; ?>
+        </div>
+        <div class="clear_box"></div>
 
-    <div id = "name_left">性別<div id=br>必須</div></div>
-    <div id = "name_right">
-    <input type="radio" name="rdo" value="男姓"  checked id="m">
-    <label for="m">男性</label>
-    <input type="radio" name="rdo" value="女姓" id = "w">
-    <label for="w">女性</label>
-    <input type="radio" name="rdo" value="不明" id="u">
-    <label for="u">不明</label>
+        <div id = "name_left">性別</div>
+        <div id = "name_right">
+            <?php
 
-    </div>
-    <div class="clear_box"></div>
-    <div id = "name_left">住所<div id=br>必須</div></div>
-    <div id = "name_right">
-    <input type="text" name="adress" placeholder = "例）東京都〇〇区〇-〇-〇" value=""　required="required">
-    </div>
-    <div class="clear_box"></div>
+            /*男性*/
 
-    <div id = "name_left">電話番号<div id=br>必須</div></div>
-    <div id = "name_right">
-    <input type="text" name="tell" placeholder = "例）090" value="" required="required">-
+            if ($_POST["rdo"] == 0) { //男性なら
+                print "男性";
+                }
 
-    <input type="text" name="tell2" placeholder = "例）000" value="" required="required">-
+           /*女性*/
 
-    <input type="text" name="tell3" placeholder = "例）000" value="" required="required">
-    </div>
+           elseif ($_POST["rdo"] == 1) { //女性なら
+                 print "女性";
+                }
 
-    <div id = "name_left">メールアドレス<div id=br>必須</div></div>
-    <div id = "name_right">
-    <input type="text" name="mail" placeholder = "例）test" value="" required="required">@
-    <input type="text" name="mail2" placeholder = "例）gmail.com" value="" required="required">
-    </div>
-    <div class="clear_box"></div>
+             /*不明*/
+            /*elseif ($_POST["rdo"] == 2) { //不明なら
 
-    <div id = "name_left">どこで知ったか(複数可)</div>
-    <div id = "name_right">
-    <input type="checkbox" name="cbx[0]" id = "c" value="雑誌">
-    <label for="c">雑誌</label>
-    <input type="checkbox" name="cbx[1]" id = "b" value="インターネット">
-    <label for="b">インターネット</label>
-    <input type="checkbox" name="cbx[2]" value="学校" id = "x">
-    <label for="x">学校</label>
-    </div>
-    <div class="clear_box"></div>
+                print "不明";
+            }*/
+            else {
+                print "不明";
+            }
 
-    <div id = "name_left">質問のカテゴリ<div id=br>必須</div></div>
-    <div id = "name_right">
-    <select name="slt">
-        <option>クレーム</option>
-        <option>相談</option>
-        <option>感想</option>
-        <option>その他</option>
-    </select>
-    </div>
-    <div class="clear_box"></div>
+         ?>
 
-    <div id = "name_left">質問内容<div id=br>必須</div></div>
-    <div id = "name_right">
-    <textarea rows="2" cols="40" name="question" placeholder = "例）パソコンが壊れました" value="" required="required"></textarea>
-    </div>
-    <div class="clear_box"></div>
 
-    <input type="submit">
-    <input type="reset" >
 
-</form>
-</div>
+         </div>
+            <div class="clear_box"></div>
+
 </body>
+</head>
 </html>
